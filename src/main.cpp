5348a9c9ad248d34e726d7bfbaa8088dd0e4f91d
T ShwethaSharan <shwethasharan.office@gmail.com>
2026-02-28 21:53:05 +0530

    pipeline working updated with additional components

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,14 @@
 
 void setup() {
     pinMode(2, OUTPUT);
-    pinMode(4, INPUT);
-    Serial.begin(115200);
-    pinMode(5, OUTPUT);
 }
 
 void loop() {
-    // Ultrasonic HC-SR04 measurement
-    digitalWrite(2, LOW);
-    delayMicroseconds(2);
-    digitalWrite(2, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(2, LOW);
-    long duration = pulseIn(4, HIGH);
-    float distance_cm = duration * 0.034 / 2.0;
-    Serial.print("Distance: ");
-    Serial.print(distance_cm);
-    Serial.println(" cm");
-    if (distance_cm < 15) {
-        Serial.println("OBJECT DETECTED!");
+    for (int i = 0; i < 100; i++) {
+        digitalWrite(2, HIGH);
+        delay(200);
+        digitalWrite(2, LOW);
+        delay(200);
     }
-    delay(200);
-    digitalWrite(5, HIGH);
-    delay(0);
-    digitalWrite(5, LOW);
-    delay(0);
+    while(true);  // halt after 100 blinks
 }
